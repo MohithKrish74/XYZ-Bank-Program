c591f8737f73efe91e6c307fca85856c79f36f21
MohithKrish74 <87643124+MohithKrish74@users.noreply.github.com>
2021-09-04 14:36:31 +0530

    Add files via upload

---
+++ XYZ Bank/Controller/Operations.java
@@ -0,0 +1,34 @@
+package com.keyword;
+
+public class Operations
+{
+    public void balance(Account myAccount)
+    {
+        System.out.println("Balance USD "+myAccount.getAccountBalance());
+
+    }
+    public void deposit(Account myAccount,double deposits)
+    {
+        double depositAfter = myAccount.getAccountBalance()+deposits;
+        myAccount.setAccountBalance(depositAfter);
+        System.out.println("Deposited Successfully!!!");
+    }
+    public void withdraw(Account myAccount,double withdraw)
+    {
+        Bank bank = new Bank();
+        double charges = 0;
+        if (withdraw % bank.CASH_WITHDRAWAL_MULTIPLE != 0) {
+            System.out.println("Please correct the amount of value. It should be multiple of USD "+bank.CASH_WITHDRAWAL_MULTIPLE);
+        } else {
+            charges = ((withdraw<= bank.MINIMUM_BALANCE) ? (bank.CHARGES * withdraw) : (2 * withdraw * bank.CHARGES));
+            if (myAccount.getAccountBalance() > withdraw && myAccount.getAccountBalance() > bank.MINIMUM_BALANCE) {
+                double withdrawAfter = myAccount.getAccountBalance() - withdraw - charges;
+                myAccount.setAccountBalance(withdrawAfter);
+                System.out.println("Charges USD " + charges);
+                System.out.println("Withdrawn Successfully!!!");
+            } else {
+                System.out.println("Insufficient money in the account.Minimum balance in the bank should be USD 100. ");
+            }
+        }
+    }
+}
